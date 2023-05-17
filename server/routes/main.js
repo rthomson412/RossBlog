9a8d084e1675407fe1b220182dbddcc16de76536
Ross Thomson <rthomson412@gmail.com>
2023-05-17 13:14:29 -0400

    about

--- server/routes/main.js
+++ server/routes/main.js
@@ -5,8 +5,8 @@ router.get('', (req, res) => {
     res.render('index');
 });
 
-// router.get('/about', (req, res) => {
-//     res.render('about');
-// });
+router.get('/about', (req, res) => {
+    res.render('about');
+});
 
 module.exports = router;
