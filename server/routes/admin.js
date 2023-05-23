38744228a50f019a9df31959d47515ff7b0ef490
Ross Thomson <rthomson412@gmail.com>
2023-05-23 19:22:23 -0400

    user schema

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/Post'); 
+const Post = require('../models/Post');
+const User = require('../models/User');
+
+
 const adminLayout = '../views/layouts/admin';
 /**
  * GET /
@@ -8,16 +11,30 @@ const adminLayout = '../views/layouts/admin';
 */
 router.get('/admin', async (req, res) => {
     try {
-      const locals = {
-        title: "Admin",
-        description: "Created with NodeJs, Express & MongoDb."
-      }
-  
-      res.render('admin/index', { locals, layout: adminLayout });
+        const locals = {
+            title: "Admin",
+            description: "Created with NodeJs, Express & MongoDb."
+        }
+
+        res.render('admin/index', { locals, layout: adminLayout });
     } catch (error) {
-      console.log(error);
+        console.log(error);
     }
-  });
+});
+
+/**
+ * POST /
+ * Admin - Check Login
+*/
+router.post('/admin', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        console.log(req.body);
+        res.render('admin/index', { locals, layout: adminLayout });
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 
 module.exports = router;
