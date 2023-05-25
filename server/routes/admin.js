f617414fa13fa2b6ab47a6371f9e26efef2205e6
Ross Thomson <rthomson412@gmail.com>
2023-05-25 19:08:37 -0400

    password hashing

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 const adminLayout = '../views/layouts/admin';
@@ -24,17 +26,27 @@ router.get('/admin', async (req, res) => {
 
 /**
  * POST /
- * Admin - Check Login
+ * Admin - Register
 */
-router.post('/admin', async (req, res) => {
+router.post('/register', async (req, res) => {
     try {
-        const { username, password } = req.body;
-        console.log(req.body);
-        res.redirect('admin')
+      const { username, password } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      try {
+        const user = await User.create({ username, password:hashedPassword });
+        res.status(201).json({ message: 'User Created', user });
+      } catch (error) {
+        if(error.code === 11000) {
+          res.status(409).json({ message: 'User already in use'});
+        }
+        res.status(500).json({ message: 'Internal server error'})
+      }
+  
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-});
+  });
 
 
 module.exports = router;
